8e99e8ac5acc7e87f7c2611ab8321a6fc53cf586
Andriikryl <andreyk557@gmail.com>
2023-07-02 17:47:20 +0300

    add button controls to the item card

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,5 @@
+import { formatCurrency } from "../utilites/formatCurency";
+
 type StoreItemProps = {
   id: number;
   name: string;
@@ -6,6 +8,7 @@ type StoreItemProps = {
 };
 
 export default function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const quntity = 0;
   return (
     <div>
       <img src={imgUrl} alt="image" width="300" height="300" />
@@ -13,7 +16,25 @@ export default function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         <span>{name}</span>
       </div>
       <div>
-        <span>{price}</span>
+        <span>{formatCurrency(price)}</span>
+      </div>
+      <div>
+        {quntity === 0 ? (
+          <button>+ add to cart</button>
+        ) : (
+          <div>
+            <div>
+              <button>plus</button>
+              <div>
+                <span>{quntity}</span>
+              </div>
+              <button>minus</button>
+              <div>
+                <button>remove</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
